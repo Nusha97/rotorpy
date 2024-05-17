db882e3b5043a4ed5209f520e20e4f3fda270872
Anusha Srikanthan <anusha.srikanthan8@gmail.com>
2024-05-17 15:03:30 -0400

    Additional code snippets needed for DATT

--- rotorpy/learning/quadrotor_reward_functions.py
+++ rotorpy/learning/quadrotor_reward_functions.py
@@ -29,3 +29,20 @@ def hover_reward(observation, action, weights={'x': 1, 'v': 0.1, 'w': 0, 'u': 1e
     action_reward = -weights['u']*np.linalg.norm(action)
 
     return dist_reward + vel_reward + action_reward + ang_rate_reward
+
+
+def trajectory_reward(observation, action, weights={'x': 1, 'v': 0.1, 'w': 0, 'u': 1e-5}):
+    """
+    Rewards used in DATT - TODO : change code to be compatible with rotorpy observations
+    """
+
+    yaw = observation.rot.as_euler('ZYX')[0]
+    yawcost = 0.5 * min(abs(self.ref.yaw(self.t) - yaw), abs(self.ref.yaw(self.t) - yaw))
+    poscost = np.linalg.norm(state.pos - self.ref.pos(self.t))#min(np.linalg.norm(state.pos), 1.0)
+    velcost = 0.1 * min(np.linalg.norm(state.vel), 1.0)
+
+    ucost = 0.2 * abs(action[0]) + 0.1 * np.linalg.norm(action[1:])
+
+    cost = yawcost + poscost + velcost
+
+    return -cost
