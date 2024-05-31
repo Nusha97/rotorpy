1bdaf99f12ca21f25aea8c273dbd18e19d23144c
Anusha Srikanthan <anusha.srikanthan8@gmail.com>
2024-05-30 21:10:16 -0400

    Trained our own RL policy in the env for trajectory tracking

--- rotorpy/learning/__init__.py
+++ rotorpy/learning/__init__.py
@@ -2,5 +2,6 @@ from gymnasium.envs.registration import register
 
 register(
      id="Quadrotor-v0",
+     # entry_point="rotorpy.learning.quadrotor_environments:QuadrotorEnv",
      entry_point="rotorpy.learning.quadrotor_environments:QuadrotorEnv",
 )
--- rotorpy/learning/quadrotor_reward_functions.py
+++ rotorpy/learning/quadrotor_reward_functions.py
@@ -5,6 +5,7 @@ import gymnasium as gym
 from gymnasium import spaces
 
 import math
+from rotorpy.utils.helper_functions import compute_yaw_from_quarternion
 
 """
 Reward functions for quadrotor tasks. 
@@ -30,19 +31,62 @@ def hover_reward(observation, action, weights={'x': 1, 'v': 0.1, 'w': 0, 'u': 1e
 
     return dist_reward + vel_reward + action_reward + ang_rate_reward
 
+# def trajectory_reward(observation, action, weights={'x': 1, 'v': 0.1, 'w': 0, 'u': 1e-5}):
+#     q = observation['w']
+#     R_matrices = R.from_quat(q).as_matrix()
+#     b3 = R_matrices[:, 2]
+#     H = np.array([[
+#                     1 - (b3[0] ** 2) / (1 + b3[2]),
+#                     -(b3[0] * b3[1]) / (1 + b3[2]),
+#                     b3[0],
+#                 ],
+#                 [
+#                     -(b3[0] * b3[1]) / (1 + b3[2]),
+#                     1 - (b3[1] ** 2) / (1 + b3[2]),
+#                     b3[1],
+#                 ],
+#                 [-b3[0], -b3[1], b3[2]],
+#             ]
+#         )
+#     Hyaw = np.transpose(H) @ R_matrices
+#     yaw = np.arctan2(Hyaw[1, 0], Hyaw[0, 0])
+#     # yaw = state.rot.as_euler('ZYX')[0]
+#     yawcost = 0.5 * min(abs(self.ref.yaw - yaw), abs(self.ref.yaw(self.t) - yaw))
+#     yawcost = 0.5 * min(abs(self.ref.yaw(self.t) - yaw), abs(self.ref.yaw(self.t) - yaw))
+#     poscost = np.linalg.norm(obs[0:3] - self.ref.x_poly(self.t))#min(np.linalg.norm(state.pos), 1.0)
+#     velcost = 0.1 * min(np.linalg.norm(obs[3:6]), 1.0)
 
-def trajectory_reward(observation, action, weights={'x': 1, 'v': 0.1, 'w': 0, 'u': 1e-5}):
+#     ucost = 0.2 * abs(action[0]) + 0.1 * np.linalg.norm(action[1:])
+
+#     cost = yawcost + poscost + velcost
+
+#     return -cost
+
+def trajectory_reward(observation, action, weights={'x': 1, 'v': 0.1, 'q': 0, 'u': 1e-5}):
     """
-    Rewards used in DATT - TODO : change code to be compatible with rotorpy observations
+    Rewards for trajectory tracking given observations following DATT. It is a combination of position error, 
+    velocity error, body rates, and action reward.
+    observation: dict(R_t^T)
     """
 
-    yaw = observation.rot.as_euler('ZYX')[0]
-    yawcost = 0.5 * min(abs(self.ref.yaw(self.t) - yaw), abs(self.ref.yaw(self.t) - yaw))
-    poscost = np.linalg.norm(state.pos - self.ref.pos(self.t))#min(np.linalg.norm(state.pos), 1.0)
-    velcost = 0.1 * min(np.linalg.norm(state.vel), 1.0)
+    # Compute position error from nearest waypoint
 
-    ucost = 0.2 * abs(action[0]) + 0.1 * np.linalg.norm(action[1:])
+    # tracking_cost = regularizer(observation[13:])  # TODO for the future
 
-    cost = yawcost + poscost + velcost
+    # Compute the distance to goal
+    dist_reward = -weights['x']*np.linalg.norm(observation[0:3]-observation[13:16])
 
-    return -cost
+    # Compute the velocity reward
+    vel_reward = -weights['v']*np.linalg.norm(observation[3:6]-observation[16:19])
+
+    # Compute the yaw reward
+    yaw = compute_yaw_from_quaternion(observation[6:10])
+    yaw_reward = -weights['q'] * np.linalg.norm(yaw - observation[28:31])
+
+    # Compute the angular rate reward
+    ang_rate_reward = -weights['w'] * np.linalg.norm(observation[10:13] - observation[31:34])
+
+    # Compute the action reward
+    action_reward = -weights['u']*np.linalg.norm(action)
+
+    return dist_reward + vel_reward + yaw_reward + action_reward + ang_rate_reward
